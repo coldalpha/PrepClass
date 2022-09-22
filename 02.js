c0e8d6774c6e9ace460a46cb2df8d2710715c862
alphacold07 <70317938+alphacold07@users.noreply.github.com>
2022-09-23 05:34:17 +0700

    Menambahkan Jawaban Soal Kedua

---
+++ 02.js
@@ -0,0 +1,12 @@
+function ganjilGenap(input) {
+    if (input % 2 == 0) {
+        console.log("Input  : " + input);
+        console.log("Output : Genap");
+    } else {
+        console.log("input  : " + input);
+        console.log("Output : Ganjil");
+    }
+}
+
+ganjilGenap(43);
+ganjilGenap(1032);
